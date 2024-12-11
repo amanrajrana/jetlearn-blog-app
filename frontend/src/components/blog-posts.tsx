b57ccb54c632ac2d566930e456bb4a575a45fe6e
Aman Raj Rana <amanrajrana.dev@gmail.com>
2024-12-11 16:37:46 +0530

    feat: add loading state and error handling to blog posts component; update API endpoint to production

--- frontend/src/components/blog-posts.tsx
+++ frontend/src/components/blog-posts.tsx
@@ -2,20 +2,31 @@ import postService from "@/services/post.services";
 import PostCard from "./post-card";
 import { useEffect, useState } from "react";
 import { Post } from "@/types/blog";
+import { toast } from "@/hooks/use-toast";
+import { Loader } from "lucide-react";
 
 const BlogPosts = ({ userId }: { userId?: number }) => {
   const [posts, setPosts] = useState<Post[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
+    setLoading(true);
     postService
       .getPosts()
       .then((data) => {
         setPosts(data);
       })
-      .catch((err) => console.error(err));
+      .catch((err: Error) =>
+        toast({ description: err.message, variant: "destructive" })
+      )
+      .finally(() => setLoading(false));
   }, []);
 
-  return (
+  return loading ? (
+    <div className="w-full my-12 flex justify-center items-center">
+      <Loader className="animate-spin" />
+    </div>
+  ) : (
     <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {posts.map((post) => {
         if (userId && userId !== post.user.id) {
